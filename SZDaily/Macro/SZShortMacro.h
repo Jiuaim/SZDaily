631fc18eb048b4e55cc85dcea107d048c14cc066
hsz <shengzhong.huang@zhangmen.com>
2021-01-21 17:50:30 +0800

    pod debug

--- SZDaily/Macro/SZShortMacro.h
+++ SZDaily/Macro/SZShortMacro.h
@@ -12,5 +12,11 @@
 #define WeakObj(o) autoreleasepool{} __weak typeof(o) o##Weak = o;
 #define StrongObj(o) autoreleasepool{} __strong typeof(o) o = o##Weak;
 
+#ifdef DEBUG
+    #define NSLog(format, ...)          printf("\n[%s] %s [第%d行]\n%s\n", __TIME__, __FUNCTION__, __LINE__, [[NSString stringWithFormat:format, ## __VA_ARGS__] UTF8String])
+#else
+    #define NSLog(format, ...)
+#endif
+
 
 #endif /* SZShortMacro_h */
